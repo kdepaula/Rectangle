5a30f781918f352aa27e6f745fd8df46905b22f9
kdepaula <42978472+kdepaula@users.noreply.github.com>
2018-10-02 14:29:47 -0400

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,24 @@
+/**
+ * Katelyn DePaula Period 7
+ */
+import java.util.Scanner;
+
+public class Rectangle {
+
+	public static void main(String[] args) 
+	{
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("What is the length?");
+		double length = scan.nextInt();
+		System.out.print("What is the width?");
+		double width = scan.nextInt();
+		double area = length * width;;
+		System.out.println("The area of the rectangle is: ");
+		
+		
+		
+		
+	}
+
+}
